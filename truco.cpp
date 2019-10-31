4ab02977fd99e368809481ee6029b5e166827a02
Giovanni Alvarenga <56125274+GiovanniAVS@users.noreply.github.com>
2019-10-31 16:37:59 -0300

    criando deck random e  construtor de card
    Seguinte, eu não conseguir racionar como eu faria um vector de classe, buguei nisso, então fiz o seguinte, no próprio construtor do round (sujeito a mudança pra o rand_deck( )) eu crie o deck usando o vector deck. O tamanho do vector é de 80, nesse vector vc tem o código do naipe e código para o valor da carta, está em ordem, começando pelo naipe. Ex: 0 2 3 7 2 8 -> paus 2, copas 7, ouros dama. Depois ele já reduz esse vector para o total de cartas que vai ser usado no round (12), mas como tem junto o naipe o vector vai ter 24 valores. Agr falta só ligar isso na list<card>hand do player.

--- truco.cpp
+++ truco.cpp
@@ -1,8 +1,4 @@
 include "truco.h"
-card::card(int avalor, std::string asuit){
-    valor = avalor;
-    suit = asuit;
-};
 std::string card::get_suit(){
     return suit;
 };
@@ -14,4 +10,74 @@ bool card::compare_valor(card c){
         return true;
     }
     return false;
+}
+
+round::round(){
+
+srand(time(NULL));
+vector<int>cc(40);
+vector<int>cn(4);
+ 
+	int i,j,num;
+	for ( i = 0; i < 10; i++){
+		cc[i] = 0;
+	}
+
+	for (j=0; j<4; j++){ 
+		for(i=0; i<10; i++){
+			num = rand()%10;
+			while(cc[num] >= 4 ){
+				num = rand()%10;
+			}
+			cc[num]++;
+			deck.push_back(num);
+		}
+	}
+
+	for (i = 0; i < (deck.size()); ++i){
+		cout<< deck[i] <<" ";
+	}
+
+	cout<<endl;
+
+	for ( i = 0; i < 4; i++){
+		cn[i] = 0;
+	}
+	for(i=0; i<40; i++){
+		num = rand()%4;
+		while(cn[num] >= 10 ){
+			num = rand()%4;	
+		}
+		cn[num]++;
+		deck.insert((deck.begin()+(i+i)),(num));
+			
+	}
+	for (i = 0; i < (deck.size()); ++i){
+		cout<< deck[i] <<" ";
+	}
+
+	for(i=0; i<56; i++){
+		deck.erase(deck.begin());	
+	}
+
+	cout<<endl;
+
+	for (i = 0; i < (deck.size()); ++i){
+		cout<< deck[i] <<" ";
+	}
+
+}	
+
+
+card::card (int v, int s){ 
+
+	if (s=0)
+			suit = "Paus";
+	if (s=1)
+		suit = "Espadas";
+	if (s=2)
+		suit = "Ouros";
+	if (s=3)
+		suit = "Copas";
+	valor = v;
 }
