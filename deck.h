bb382e7aa876bbece083d3d265ac54358489684a
Giovanni Alvarenga <56125274+GiovanniAVS@users.noreply.github.com>
2019-11-12 17:39:08 -0300

    Update deck.h

--- deck.h
+++ deck.h
@@ -1,1 +1,18 @@
+#ifndef DECK_H_INCLUDED
+#define DECK_H_INCLUDED
+#include <cmath>
+#include <string>
+#include <map>
+#include <vector>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include"bot.h"
+#include"player.h"
+#include"humano.h"
+#include"hand.h"
+#include"card.h"
+#include"menu.h"
+#include"game.h"
 
+using namespace std;
