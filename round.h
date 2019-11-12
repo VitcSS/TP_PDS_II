b820c1d6482fbae1717c2599bbe384daacc66f81
VitcSS <52133287+VitcSS@users.noreply.github.com>
2019-11-12 17:46:21 -0300

    Update round.h

--- round.h
+++ round.h
@@ -1,1 +1,18 @@
+#include <cmath>
+#include <string>
+#include <map>
+#include <vector>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include"bot.h"
+#include"player.h"
+#include"humano.h"
+#include"hand.h"
+#include"card.h"
+#include"menu.h"
+#include"deck.h"
+#include"game.h"
+
+using namespace std;
 
