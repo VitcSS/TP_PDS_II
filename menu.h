d4ec0e99e7484630c01b7d8a3df8c31e0ff119ff
Henrique Alves Barbosa <henriquealvesbarbosa@MacBook-de-Henrique.local>
2019-11-12 18:08:31 -0300

    add classe menu

--- menu.h
+++ menu.h
@@ -14,5 +14,21 @@
 #include"card.h"
 #include"deck.h"
 #include"game.h"
-
-using namespace std;
+class menu{
+    private:
+        int num_players;
+        std::list<char> players;
+    public:
+        menu(int num_players){
+            this->num_players = num_players;
+        };
+        void add_players(){
+            for(int i =0; i<this->num_players; i++){
+                std::cout<<"Entre com o nickname"<<std::endl;
+                char nick;
+                std::cin>>nick;
+                this->players.push_back(nick);
+            };
+        };
+    
+};
