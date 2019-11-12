cfd087f45dc47626c60360b83a61d1081de8f93b
Giovanni Alvarenga <56125274+GiovanniAVS@users.noreply.github.com>
2019-11-12 17:41:48 -0300

    Update hand.h

--- hand.h
+++ hand.h
@@ -1,1 +1,18 @@
+#ifndef HAND_H_INCLUDED
+#define HAND_H_INCLUDED
+#include <cmath>
+#include <string>
+#include <map>
+#include <vector>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include"bot.h"
+#include"player.h"
+#include"humano.h"
+#include"card.h"
+#include"menu.h"
+#include"deck.h"
+#include"game.h"
 
+using namespace std;
