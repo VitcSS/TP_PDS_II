0098b4d751797ab2413587cd5ece6dd916fe0daf
Giovanni Alvarenga <56125274+GiovanniAVS@users.noreply.github.com>
2019-11-12 17:44:17 -0300

    Update menu.h

--- menu.h
+++ menu.h
@@ -1,1 +1,18 @@
+#ifndef MENU_H_INCLUDED
+#define MENU_H_INCLUDED
+#include <cmath>
+#include <string>
+#include <map>
+#include <vector>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include"bot.h"
+#include"player.h"
+#include"humano.h"
+#include"hand.h"
+#include"card.h"
+#include"deck.h"
+#include"game.h"
 
+using namespace std;
