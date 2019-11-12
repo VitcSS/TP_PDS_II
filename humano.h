8893a5f969a3022d8b35f671a2904476e0e7f2a5
Giovanni Alvarenga <56125274+GiovanniAVS@users.noreply.github.com>
2019-11-12 17:43:01 -0300

    Update humano.h

--- humano.h
+++ humano.h
@@ -1,1 +1,25 @@
+#ifndef HUMANO_H_INCLUDED
+#define HUMANO_H_INCLUDED
+#include <cmath>
+#include <string>
+#include <map>
+#include <vector>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include"bot.h"
+#include"player.h"
+#include"hand.h"
+#include"card.h"
+#include"menu.h"
+#include"deck.h"
+#include"game.h"
 
+class human : public player{
+    public:
+        human(std::string name, std::list<card> hand);
+
+    private:
+        std::string name;
+        std::list<card> hand;
+};
