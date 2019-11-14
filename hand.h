41db77e614bc5c1e4c2732b6b04475c08a1756f2
VitcSS <52133287+VitcSS@users.noreply.github.com>
2019-11-14 09:37:00 -0300

    Adicionada a função burn_hand

--- hand.h
+++ hand.h
@@ -21,7 +21,7 @@ class hand: public deck(){
   private:
   
   public:
-  void play_card(round r, int selec){
+  void play_card(round *r, int selec){
     if(selec == 0)
       return;
     if(selec > this->deck.size() || selec < 0){
@@ -32,7 +32,7 @@ class hand: public deck(){
     else if(selec>0){
       list<card> iterator = deck.begin();
       advance(iterator, (selec-1));
-      r.played.deck.push_back(*(iterator));
+      *(r.played.deck.push_back(*(iterator)));
       this->deck.erase(iterator);// Conferir funcionamento da função.
       return;
     }
@@ -40,4 +40,13 @@ class hand: public deck(){
   int get_size_hand(){
     return deck.size();
   }
-}
+  void burn_hand(round *r){
+  list<card> iterator = deck.begin();  
+  for(int i = 1; i =< (this->deck.size()); i++){
+    advance(iterator, 1);
+    *(r.deck.deck.push_back(*(iterator)));
+  }
+    this->deck.erase(deck.begin(), deck.end());
+    return
+  }
+};
