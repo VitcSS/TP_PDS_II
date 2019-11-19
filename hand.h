778481f1746f672b3516c0e28660ac51176a6839
VitcSS <52133287+VitcSS@users.noreply.github.com>
2019-11-19 17:39:42 -0300

    Update hand.h

--- hand.h
+++ hand.h
@@ -24,29 +24,29 @@ class hand: public deck(){
   void play_card(round *r, int selec){
     if(selec == 0)
       return;
-    if(selec > this->deck.size() || selec < 0){
+    if(selec > this->Deck.size() || selec < 0){
       cout<<"A carta de número "<<selec<<" não existe!!!"<<endl;
-      cout<<"Seu número de cartas é: "<<this->deck.size();
+      cout<<"Seu número de cartas é: "<<this->Deck.size();
       return;
     }
     else if(selec>0){
       list<card> iterator = deck.begin();
       advance(iterator, (selec-1));
-      *(r.played.deck.push_back(*(iterator)));
-      this->deck.erase(iterator);// Conferir funcionamento da função.
+      *(r.played.Deck.push_back(*(iterator)));
+      this->Deck.erase(iterator);// Conferir funcionamento da função.
       return;
     }
   }
   int get_size_hand(){
-    return deck.size();
+    return Deck.size();
   }
   void burn_hand(round *r){
-  list<card> iterator = deck.begin();  
-  for(int i = 1; i =< (this->deck.size()); i++){
+  list<card> iterator = Deck.begin();  
+  for(int i = 1; i =< (this->Deck.size()); i++){
     advance(iterator, 1);
-    *(r.deck.deck.push_back(*(iterator)));
+    *(r.Deck.Deck.push_back(*(iterator)));
   }
-    this->deck.erase(deck.begin(), deck.end());
+    this->Deck.erase(Deck.begin(), Deck.end());
     return
   }
 };
