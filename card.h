187e6465a403fd285d4ff15e2bacd4ad963b9cd2
Henrique Alves Barbosa <henriquealvesbarbosa@MacBook-de-Henrique.local>
2019-11-27 18:47:07 -0300

    arrumar card

--- card.h
+++ card.h
@@ -7,14 +7,6 @@
 #include <iomanip>
 #include <iostream>
 #include <list>
-#include"bot.h"
-#include"player.h"
-#include"humano.h"
-#include"hand.h"
-#include"card.h"
-#include"menu.h"
-#include"deck.h"
-#include"game.h"
 
 using namespace std;
 
