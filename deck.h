1e10a8a220b86b17971e8896b59dcd9e9c4e1dba
Henrique Alves Barbosa <henriquealvesbarbosa@MacBook-de-Henrique.local>
2019-11-27 19:58:20 -0300

    ARRUMANDO MAIS

--- deck.h
+++ deck.h
@@ -21,7 +21,6 @@ public:
   deck();
   // ~deck();
   void shuffle(vector<card> );
-  int myrandom(int );
   friend class round;
 };
 
@@ -61,72 +60,4 @@ void deck::shuffle(vector <card> Deck){
 
 #endif
 
-/*
-
-#include <cmath>
-#include <string>
-#include <map>
-#include <vector>
-#include <iomanip>
-#include <iostream>
-#include <list>
-#include <algorithm>
-//#include "deck.h"
-#include "card.h"
-//#include "round.h"
-
-
-using namespace std;
-
-
-
-
-int main(){
-
-
-  vector<card> Deck;
-  void shuffle(vector<card> );
-  int myrandom(int );
-
-
-
-srand(time(NULL));
-int i = 0;
-while (i < 10) {
-	int j = 0;
-	while (j < 4) {
-		Deck.push_back(card(i, j));
-		j++;
-	};
-	i++;
-};
-
-
-shuffle(Deck);
-
-
-};
-
-
-
-
-
-int myrandom (int i) { return std::rand()%i;};
-
-
-void shuffle(vector <card> Deck){
-	std::srand ( unsigned ( std::time(NULL) ) );
-	std::random_shuffle ( Deck.begin(), Deck.end(), myrandom);
-	vector<card>::iterator iter = Deck.begin();
-    for (; iter != Deck.end(); iter++) {
-	    iter->imprimir();
-	    cout << endl;    
-    };
-};
-
-
-
-
-
-*/
 
