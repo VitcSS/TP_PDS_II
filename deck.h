5d84f79b7c622eeea36f8dd814de0ae4778ce088
Giovanni Alvarenga <56125274+GiovanniAVS@users.noreply.github.com>
2019-12-02 14:19:14 -0300

    tentando criar a mão dentro do deck

--- deck.h
+++ deck.h
@@ -16,12 +16,14 @@ using namespace std;
 class deck{
 private:
   vector<card> Deck;
+  vector<card> Hand;
 
 public:
   deck();
   // ~deck();
   void shuffle(vector<card> );
   int myrandom(int );
+  void create_hand(vector<card>);
   friend class round;
   friend class hand;
 };
@@ -39,23 +41,43 @@ while (i < 10) {
 	i++;
 };
 
+cout<<"teste";
 shuffle(Deck);
+
+
 };
 
+
 int deck::myrandom (int i) { return std::rand()%i;};
+
 
 void deck::shuffle(vector <card> Deck){
 	std::srand ( unsigned ( std::time(NULL) ) );
 	int num = (rand()%30) + 3;
 	for(int numero = 0; numero <= num; numero++){
-		std::random_shuffle ( Deck.begin(), Deck.end());
-	};
+		std::random_shuffle ( Deck.begin(), Deck.end());};
 	vector<card>::iterator iter = Deck.begin();
-    	for (; iter != Deck.end(); iter++) {
+    for (; iter != Deck.end(); iter++) {
 	    iter->imprimir();
 	    cout << endl;    
-    	};
+    };
+
+	cout << endl;    
+	cout << endl;    
+	cout << endl;    
+	create_hand(Deck);
+
 };
 
+void deck::create_hand(vector <card> Deck){
+	vector<card>::iterator it = Deck.begin();
+
+	for(int i=0; i<=2; i++){
+		Hand.push_back(*it);
+		advance(it,1);
+	}
+
+    
+}
 
 #endif
