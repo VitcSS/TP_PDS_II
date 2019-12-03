8bff95ea5086617902d5c7df6d1b61f22980dac1
Henrique Alves Barbosa <henriquealvesbarbosa@MacBook-de-Henrique.local>
2019-12-03 17:54:38 -0300

    sdgdnbudfvoufnvufn

--- round.h
+++ round.h
@@ -18,11 +18,15 @@ class round{
         int primeiro;
         int ganhador;
     public:
-        round(int i);
+        round(int);
         vector<card> get_mesa();
         int get_ganhador();
         void add_carta();
         void limpar();
 };
+
+round::round(int primeiro){
+    this->primeiro = primeiro;
+}
 
 #endif
