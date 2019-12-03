5a1c620a0cfcb7fc85044bed6384d87f8a80cdf8
root <root@localhost.localdomain>
2019-12-03 18:39:49 -0200

    Nada e permanente

--- truco.cpp
+++ truco.cpp
@@ -1,108 +1,53 @@
-#include "truco.h"
 #include <iostream>
-#include <iomanip>
-#include <math.h>
-#include <list>
-#include <vector>
+#include <utility>
+#include "truco.h"
 
-std::string card::get_suit(){
-    return suit;
-};
-int card::get_valor(){
-    return valor;
-};
-bool card::compare_valor(card c){
-    if(this->suit > c.get-suit()){
-        return true;
-    }
-    return false;
-}
-
-
-card::card (int v, int s){ 
-	switch (s) {
-		case 0:
-			suit = "Paus";
-			break;
-		case 1:
-			suit = "Espadas";
-			break;
-		case 2:
-			suit = "Ouros";
-			break;
-		case 3:
-			suit = "Copas";
-			break;
+int main() {
+	//Inicialização do baralho e criação dos jogadores:
+	deck D = deck();
+	menu M = menu();
+	vector<player*> jogadores;
+	pair<int, int> par = M.get_jogadores();
+	if (par.second == 0) {
+		for (int i = 0; i < par.first; i++) {
+			string nome;
+			cin.clear();
+			fflush(stdin);
+			cout << "Diga o nome do humano " << i + 1 << ": ";
+			cin >> nome;
+			jogadores.push_back(new human(nome, D));
+		}
+		for (int j = par.first; j < 4; j++) {
+			jogadores.push_back(new bot(D));
+		}
+	} else if (par.second == 1) {
+		for (int i = 0; i < 2; i++) {
+			string nome;
+			cin.clear();
+			fflush(stdin);
+			cout << "Diga o nome do humano " << i + 1 << ": ";
+			cin >> nome;
+			jogadores.push_back(new human(nome, D));
+			jogadores.push_back(new bot(D));
+		}
+	} else {
+		for (int i = 0; i < 2; i++) {
+			string nome;
+			cin.clear();
+			fflush(stdin);
+			cout << "Diga o nome do humano " << i + 1 << ": ";
+			cin >> nome;
+			jogadores.push_back(new human(nome, D));
+		} 
+		for (int i = 0; i < 2; i++) {
+			jogadores.push_back(new bot(D));
+		}
 	}
-	valor = v;
-	switch (valor) {
-		case 0:
-			nome = "As";
-			break;
-		case 1:
-			nome = "Dois";
-			break;
-		case 2:
-			nome = "Tres";
-			break;
-		case 3:
-			nome = "Quatro";
-			break;
-		case 4:
-			nome = "Cinco";
-			break;
-		case 5:
-			nome = "Seis";
-			break;
-		case 6:
-			nome = "Sete";
-			break;
-		case 7:
-			nome = "Dama";
-			break;
-		case 8:
-			nome = "Valete";
-			break;
-		case 9:
-			nome = "Rei";
-			break;
-	}
-}
 
-//excluir depois
-void card::imprimir() {
-	cout << nome << " de " << suit;
-}
+	//while(jogadores[0]->get_pontos() < 12 && jogadores[1]->get_pontos() < 12) {
+		//Round R = Round(0);
+		//D = deck();
+		//atualizar(jogadores, D);
 
-int main(){
-
-srand(time(NULL));
-vector<card> deck;
-int i = 0;
-while (i < 10) {
-	int j = 0;
-	while (j < 4) {
-		deck.push_back(card(i, j));
-		j++;
-	}
-	i++;
-}
-
-vector<card>::iterator iter = deck.begin();
-for (; iter != deck.end(); iter++) {
-	iter->imprimir();
-	cout << endl;
-}
-	
-"Construtor de um humano: recebe name como o nome que ele define e hand como as cartas que possui"
-human::human(std::string name, std::list<card> hand){
-	this->name = name;
-	this->hand = hand;
-}
-"Atributo static que armazena o numero de bots no jogo"
-int bot::robot_numers = 0;
-"construtor de bots: id é a referencia para como identificar o bot e aumenta o numero de robos"
-void bot::set_id(){
-	this->id = this->robot_numers++;
-	this->robot_numers++;
+	//}
 }
